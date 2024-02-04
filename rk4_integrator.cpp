2bd7b07a5539e3b308687363a4ea65a7ac870077
mohid169 <mohidkhan169@gmail.com>
2024-02-04 11:51:53 -0500

    Updating integrator/refactoring and debugging

--- rk4_integrator.cpp
+++ rk4_integrator.cpp
@@ -1,4 +1,6 @@
 // File RK4_integrator.cpp
+
+
 #include "rk4_integrator.hpp"
 #include <cmath>
 
@@ -8,30 +10,29 @@ const double dt = .01;
 void runRK4(CartPendulum& system, double controlForce) {
 
 // Step 1
-double k1_theta_dot = system.calculatePendulumVelocity(system.getThetaDot());
-double k1_theta_accel = system.calculateThetaAcceleration(system.getPendulumAngle(), system.getThetaDot(), system.calculateCartAcceleration(controlForce, system.getPendulumAngle(), system.getThetaDot(), system.getCartPosition(), system.getCartVelocity()));
-double k1_x_dot = system.calculateCartAcceleration(controlForce, system.getPendulumAngle(), system.getThetaDot(), system.getCartPosition(), system.getCartVelocity());
-double k1_x_accel = system.calculateCartAcceleration(controlForce, system.getPendulumAngle(), system.getThetaDot(), system.getCartPosition(), system.getCartVelocity());
+double k1_theta_dot = system.getThetaDot();
+double k1_theta_accel = system.calculateThetaAcceleration(system.getPendulumAngle(), k1_theta_dot, system.calculateCartAcceleration(controlForce, system.getPendulumAngle(), k1_theta_dot, system.getCartPosition(), system.getCartVelocity()));
+double k1_x_dot = system.calculateCartAcceleration(controlForce, system.getPendulumAngle(), k1_theta_dot, system.getCartPosition(), system.getCartVelocity());
+double k1_x_accel = system.calculateCartAcceleration(controlForce, system.getPendulumAngle(), k1_theta_dot, system.getCartPosition(), system.getCartVelocity());
 
 // Step 2
-double k2_theta_dot = system.calculatePendulumVelocity(system.getThetaDot() + k1_theta_accel * dt / 2.0);
+double k2_theta_dot = system.getThetaDot() + k1_theta_accel * dt / 2.0;
 double k2_theta_accel = system.calculateThetaAcceleration(system.getPendulumAngle() + k1_theta_dot * dt / 2.0, system.getThetaDot() + k1_theta_accel * dt / 2.0, system.calculateCartAcceleration(controlForce, system.getPendulumAngle() + k1_theta_dot * dt / 2.0, system.getThetaDot() + k1_theta_accel * dt / 2.0, system.getCartPosition() + k1_x_dot * dt / 2.0, system.getCartVelocity() + k1_x_accel * dt / 2.0));
 double k2_x_dot = system.calculateCartAcceleration(controlForce, system.getPendulumAngle() + k1_theta_dot * dt / 2.0, system.getThetaDot() + k1_theta_accel * dt / 2.0, system.getCartPosition() + k1_x_dot * dt / 2.0, system.getCartVelocity() + k1_x_accel * dt / 2.0);
 double k2_x_accel = system.calculateCartAcceleration(controlForce, system.getPendulumAngle() + k1_theta_dot * dt / 2.0, system.getThetaDot() + k1_theta_accel * dt / 2.0, system.getCartPosition() + k1_x_dot * dt / 2.0, system.getCartVelocity() + k1_x_accel * dt / 2.0);
 
 // Step 3
-double k3_theta_dot = system.calculatePendulumVelocity(system.getThetaDot() + k2_theta_accel * dt / 2.0);
+double k3_theta_dot = system.getThetaDot() + k2_theta_accel * dt / 2.0;
 double k3_theta_accel = system.calculateThetaAcceleration(system.getPendulumAngle() + k2_theta_dot * dt / 2.0, system.getThetaDot() + k2_theta_accel * dt / 2.0, system.calculateCartAcceleration(controlForce, system.getPendulumAngle() + k2_theta_dot * dt / 2.0, system.getThetaDot() + k2_theta_accel * dt / 2.0, system.getCartPosition() + k2_x_dot * dt / 2.0, system.getCartVelocity() + k2_x_accel * dt / 2.0));
 double k3_x_dot = system.calculateCartAcceleration(controlForce, system.getPendulumAngle() + k2_theta_dot * dt / 2.0, system.getThetaDot() + k2_theta_accel * dt / 2.0, system.getCartPosition() + k2_x_dot * dt / 2.0, system.getCartVelocity() + k2_x_accel * dt / 2.0);
 double k3_x_accel = system.calculateCartAcceleration(controlForce, system.getPendulumAngle() + k2_theta_dot * dt / 2.0, system.getThetaDot() + k2_theta_accel * dt / 2.0, system.getCartPosition() + k2_x_dot * dt / 2.0, system.getCartVelocity() + k2_x_accel * dt / 2.0);
 
 // Step 4
-double k4_theta_dot = system.calculatePendulumVelocity(system.getThetaDot() + k3_theta_accel * dt);
+double k4_theta_dot = system.getThetaDot() + k3_theta_accel * dt;
 double k4_theta_accel = system.calculateThetaAcceleration(system.getPendulumAngle() + k3_theta_dot * dt, system.getThetaDot() + k3_theta_accel * dt, system.calculateCartAcceleration(controlForce, system.getPendulumAngle() + k3_theta_dot * dt, system.getThetaDot() + k3_theta_accel * dt, system.getCartPosition() + k3_x_dot * dt, system.getCartVelocity() + k3_x_accel * dt));
 double k4_x_dot = system.calculateCartAcceleration(controlForce, system.getPendulumAngle() + k3_theta_dot * dt, system.getThetaDot() + k3_theta_accel * dt, system.getCartPosition() + k3_x_dot * dt, system.getCartVelocity() + k3_x_accel * dt);
 double k4_x_accel = system.calculateCartAcceleration(controlForce, system.getPendulumAngle() + k3_theta_dot * dt, system.getThetaDot() + k3_theta_accel * dt, system.getCartPosition() + k3_x_dot * dt, system.getCartVelocity() + k3_x_accel * dt);
-double k4_x_accel = system.calculateCartAcceleration(controlForce, system.getPendulumAngle() + k3_theta_dot * dt, system.getThetaDot() + k3_theta_accel * dt, system.getCartPosition() + k3_x_dot * dt, system.getCartVelocity() + k3_x_accel * dt);
- 
+
  
 system.update(dt, controlForce,
                   k1_x_dot, k2_x_dot, k3_x_dot, k4_x_dot,
--- rk4_integrator.hpp
+++ rk4_integrator.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
-#include "cart_pendulum.hpp"
+#ifndef RK4_INTEGRATOR_HPP
 #include <cmath>
+#include "cart_pendulum.hpp"
+
 
 
 // Function to perform RK4 integration for the CartPendulum system
 void runRK4(CartPendulum& system, double controlForce);
+#endif // RK4_INTEGRATOR_HPP
