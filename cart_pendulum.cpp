b1df963f68b75beedbaa2bac46d7eabc2731aaa7
mohid169 <mohidkhan169@gmail.com>
2024-05-10 17:11:41 -0500

    Fixed dynamics :)))

--- cart_pendulum.cpp
+++ cart_pendulum.cpp
@@ -10,12 +10,14 @@ const double g=9.81;
 //Constructor for CartPendulum class
 CartPendulum::CartPendulum(const double cartMass, const double pendulumMass, const double pendulumLength)
     : M(cartMass), m(pendulumMass), l(pendulumLength),
-      x(0.0), z(pendulumLength), theta(0.11), x_dot(0.0), z_dot(0.0), theta_dot(0.0) {}
+      x(0.0), z(pendulumLength), theta(0), x_dot(0.0), z_dot(0.0), theta_dot(0.0) {}
 
+//delete 'z' parameter, not used for anything
  
  void CartPendulum::update(double controlForce, double dt){
-    double x_double_dot =  calculateCartAcceleration(controlForce, theta, theta_dot, x, x_dot);
-    double theta_double_dot = calculateThetaAcceleration(controlForce,  theta_dot, x_double_dot, controlForce);
+    double x_double_dot =  calculateCartAcceleration(controlForce, theta, theta_dot, x, x_dot); 
+    //Change function signature to be not shit
+    double theta_double_dot = calculateThetaAcceleration(theta,  theta_dot, x_double_dot, controlForce);
 
      // Update velocities using Euler integration
     x_dot += x_double_dot * dt;
@@ -50,8 +52,8 @@ double CartPendulum::getPendulumAngle() const{
 
 double CartPendulum::calculateCartAcceleration(double controlForce, double theta, double theta_dot, double x, double x_dot) const {
     // Calculate cart acceleration (x_double_dot)
-    double numerator = controlForce + m*sin(theta)*(l*pow(theta_dot,2)+g*cos(theta));
-    double denominator = M + m* (pow(sin(theta), 2));
+    double numerator = controlForce - m*sin(theta)*(l*pow(theta_dot,2)-g*cos(theta));
+    double denominator = M - m* (pow(sin(theta), 2));
     std::cout<<"x_accel: ";
     std::cout<<numerator/denominator<<std::endl;
     return numerator / denominator;
@@ -59,11 +61,10 @@ double CartPendulum::calculateCartAcceleration(double controlForce, double theta
 
 double CartPendulum::calculateThetaAcceleration(double theta, double theta_dot, double x_double_dot, double controlForce) const {
     // Update the dynamics based on the provided equation
-    double numerator = -1.0*controlForce*cos(theta) - m*l*pow(theta_dot,2 )*cos(theta)*sin(theta) - (M+m)*g*sin(theta);
-    double denominator = (M + m * pow(sin(theta), 2)) * l;
+    double numerator = 1.0*controlForce*cos(theta) - m*l*pow(theta_dot,2 )*cos(theta)*sin(theta) + (M+m)*g*sin(theta);
+    double denominator = (M - m * pow(sin(theta), 2)) * l;
     std::cout<<"theta accel";
     std::cout<<numerator/denominator<<std::endl;
-
     return numerator / denominator;
 }
 
